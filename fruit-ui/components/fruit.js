2d793eb8639771b1e87c90fe0cc2919c5c2da211
gregh <gregholley@hotmail.com>
2021-09-11 11:09:26 +0100

    Added Bootstrap and tidied up fruit component

--- fruit-ui/components/fruit.js
+++ fruit-ui/components/fruit.js
@@ -1,45 +1,14 @@
 import React, { Component } from 'react'
 import axios from "axios";
 
-export default class Fruit extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          fruit: []
-      };
-    }
 
-    getFruitData() {
-      axios
-        .get(`https://localhost:44311/api/Fruit/GetFruit`, {})
-        .then(res => {
-            const data = res.data
-            console.log(data)
-            const fruit = data.map(f =>
-                <div>
-                  <h4>{f.name}</h4>
-                  <span>{f.description}</span>
-                </div>
-              )
+export default function Fruit(props) {
 
-              this.setState({fruit})
+  return props.fruit.map(f =>
+    <div key={f.id}>
+      <h4>{f.name}</h4>
+      <span>{f.description}</span>
+    </div>
+  )
 
-
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-
-    componentDidMount(){
-        this.getFruitData()
-    }
-
-  render() {
-      return (
-          <div>
-              {this.state.fruit}
-          </div>
-      )
-  }
 }
--- fruit-ui/pages/index.js
+++ fruit-ui/pages/index.js
@@ -1,14 +1,39 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import fruitList from "../dummy-data";
 import Fruit from "../components/fruit"
 //import api from "../utils/API"
+import {useState, useEffect} from "react"
+import axios from "axios"
 
 import React from "react";
 
 export default function Home() {
+
+  const [fruit, setFruit] = useState([]);
+
+  function getFruitData() {
+    axios
+      .get(`https://localhost:44311/api/Fruit/GetFruit`, {})
+      .then(res => {
+          console.log(res.data)
+        setFruit(res.data)
+
+      })
+      .catch((error) => {
+          console.log(error)
+      });
+  }
+
+  useEffect(() => {
+
+    getFruitData();
+
+  },[])
+  
    return (
      <>
        <h1>Fruit</h1>
-       <Fruit></Fruit>
+       <Fruit fruit={fruit}></Fruit>
       </>
    )
  }
