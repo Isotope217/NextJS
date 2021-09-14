516d5b224a4547fa5d8aec4215844bc5bb8047ff
Greg Holley <gregholley@hotmail.com>
2021-09-14 20:49:28 +0100

    styled the app and added a fruit service loader

--- fruit-ui/components/fruit.js
+++ fruit-ui/components/fruit.js
@@ -4,10 +4,22 @@ import axios from "axios";
 
 export default function Fruit(props) {
 
-  return props.fruit.map(f =>
-    <div key={f.id}>
-      <h4>{f.name}</h4>
-      <span>{f.description}</span>
+  return props.isLoading ? <div>
+    <h1>Loading...</h1>
+  </div> : props.fruit.map(f =>
+    <div class="col mb-5">
+      <div key={f.id} class="card h-100 fruit-card" style={{"width": "18rem"}}>
+        <img src={f.image} class="card-img-top" alt="..."></img>
+        <div class="card-body p-4">
+        <div class="text-center">
+            <h5 class="fw-bolder">{f.name}</h5>
+            <p class="card-text">{f.description}</p>
+        </div>
+        </div>
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+          </div>
+      </div>
     </div>
   )
 
--- fruit-ui/pages/index.js
+++ fruit-ui/pages/index.js
@@ -4,12 +4,14 @@ import Fruit from "../components/fruit"
 //import api from "../utils/API"
 import {useState, useEffect} from "react"
 import axios from "axios"
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import React from "react";
 
 export default function Home() {
 
   const [fruit, setFruit] = useState([]);
+  const [isLoading, setLoading] = useState(true)
 
   function getFruitData() {
     axios
@@ -17,7 +19,7 @@ export default function Home() {
       .then(res => {
           console.log(res.data)
         setFruit(res.data)
-
+        setLoading(false)
       })
       .catch((error) => {
           console.log(error)
@@ -32,8 +34,40 @@ export default function Home() {
   
    return (
      <>
-       <h1>Fruit</h1>
-       <Fruit fruit={fruit}></Fruit>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Fruit Shop</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Buy some fruit</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">from the React.js fruit shop</p>
+                </div>
+            </div>
+        </header>
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <Fruit isLoading={isLoading} fruit={fruit}></Fruit>
+                </div>
+            </div>
+        </section>
+       
       </>
    )
  }
